6c61458377a22c0fc74a5bc35ab74af692e62457
yester31 <yester31@naver.com>
2021-03-28 16:38:59 +0900

    greedy 큰수의 법칙2 가장 큰 수와 두번째 큰 수가 더해지는 횟수를 구하여 계산

--- greedy2.py
+++ greedy2.py
@@ -11,16 +11,13 @@ second = data[n-2] # 두번째 큰수
 
 result = 0
 
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+# 가장큰 수가 더해지는 횟수 계산
+count = int(m/(k+1)) * k
+count += m % (k+1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second # 두 번째 큰 수 더하기
 
 print(result)
 
